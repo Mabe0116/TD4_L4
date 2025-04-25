6da8e76044dc658f658776fbfe621f7177c73bcb
k022g0004 <k022g0004@g.neec.ac.jp>
2025-04-25 14:08:21 +0900

    mapをcsvで読み込むように変更

--- Assets/Scenes/Scripts/GameManagerScript.cs
+++ Assets/Scenes/Scripts/GameManagerScript.cs
@@ -1,27 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
 {
     public GameObject block;
+
+    public TextAsset csvFile;
     int[,] map;
+
+    void LoadCSV()
+    {
+        //CSVファイルを読み込む
+        string[] lines = csvFile.text.Split('\n');
+        int height = lines.Length;
+        int width = lines[0].Split(',').Length;
+
+        map = new int[height, width];
+
+        for(int y=0;y<height;y++)
+        {
+            string[] lineData = lines[y].Trim().Split(',');
+            for(int x=0;x<width;x++)
+            {
+                int.TryParse(lineData[x], out map[y, x]);
+            }
+        }
+
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        map = new int[,]
-        {
-           {1,1,1,1,1,1,1,1,1 },
-           {1,0,0,0,0,0,0,0,1 },
-           {1,0,0,0,0,0,0,0,1 },
-           {1,0,0,0,0,0,0,0,1 },
-           {1,0,0,0,0,0,0,0,1 },
-           {1,1,1,1,1,1,1,1,1 },
-        };
+        LoadCSV();
 
-       
-        Vector3 position = Vector3.zero;
+
+         Vector3 position = Vector3.zero;
 
         //マップチップでの描画
         for (int y = 0; y < map.GetLength(0); y++)
@@ -29,13 +44,13 @@ public class GameManagerScript : MonoBehaviour
             for (int x = 0; x < map.GetLength(1); x++)
             {
                 position.x = x;
-                position.y = -y + 5;
+                position.y = -y+5;
                 if (map[y, x] == 1)
                 {
                     Instantiate(block, position, Quaternion.identity);
                 }
             
-                }
+            }
         }
     }
 
