6fb9fa1611d7d40ed752c2f71961794372ece8ae
shibuehayato <115055046+shibuehayato@users.noreply.github.com>
2025-05-08 02:37:38 +0900

    プレイヤーが動かなくなるバグを修正した

--- Assets/Scenes/Scripts/player/Player.cs
+++ Assets/Scenes/Scripts/player/Player.cs
@@ -8,48 +8,49 @@ public class Player : MonoBehaviour
     public Rigidbody rb;
     public float moveSpeed = 5.0f;
     public float moveJump = 5.0f;
-    public bool Cube = true;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-       
-    }
+    private bool isGrounded = false;
+    private float horizontalInput = 0f;
+    private bool jumpRequested = false;
 
-    // Update is called once per frame
     void Update()
     {
+        // 入力だけ検出
+        horizontalInput = 0;
+
+        if (Input.GetKey(KeyCode.D)) horizontalInput = 1;
+        else if (Input.GetKey(KeyCode.A)) horizontalInput = -1;
+
+        if ((Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.Space)) && isGrounded)
+        {
+            jumpRequested = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // 接地判定（レイキャスト）
         Vector3 rayPosition = transform.position;
         Ray ray = new Ray(rayPosition, Vector3.down);
         float distance = 0.6f;
         Debug.DrawRay(rayPosition, Vector3.down * distance, Color.red);
-        Cube = Physics.Raycast(ray, distance);
+        isGrounded = Physics.Raycast(ray, distance);
 
-        Vector3 v = rb.velocity;
-        if(Input.GetKey(KeyCode.D))
+        // 移動処理（MovePositionを使う）
+        Vector3 move = new Vector3(horizontalInput * moveSpeed * Time.fixedDeltaTime, 0f, 0f);
+        rb.MovePosition(rb.position + move);
+
+        // ジャンプ処理（AddForce）
+        if (jumpRequested)
         {
-            v.x = moveSpeed;
+            rb.AddForce(Vector3.up * moveJump, ForceMode.Impulse);
+            jumpRequested = false;
         }
-        else if(Input.GetKey(KeyCode.A))
-        {
-            v.x = -moveSpeed;
-        }
-        else
-        {
-            v.x = 0;
-        }
-        if (UnityEngine.Input.GetButton("Jump") || UnityEngine.Input.GetKey(KeyCode.Space))
-        {
-            if (Cube == true)
-            {
-                Debug.DrawRay(rayPosition, Vector3.down * distance, Color.red);
-                v.y = moveJump;
-            }
-            else
-            {
-                Debug.DrawRay(rayPosition, Vector3.down * distance, Color.yellow);
-            }
-        }
-        rb.velocity = v;
+    }
+
+    void LateUpdate()
+    {
+        // 回転のリセット（見た目だけ）
+        transform.rotation = Quaternion.Euler(0f, 0f, 0f);
     }
 }
