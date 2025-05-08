440461f6edc2163d2b4e81b45a8a2881e12e5b63
k022g0004 <k022g0004@g.neec.ac.jp>
2025-05-09 06:28:31 +0900

    hozon

---
+++ Assets/Scenes/Scripts/player/PlayerShadow.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShadow : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float moveSpeed = 5.0f;
+    public float moveJump = 5.0f;
+    public bool Cube = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rayPosition = transform.position;
+        Ray ray = new Ray(rayPosition, Vector3.down);
+        float distance = 0.6f;
+        Debug.DrawRay(rayPosition, Vector3.down * distance, Color.red);
+        Cube = Physics.Raycast(ray, distance);
+
+        Vector3 v = rb.velocity;
+        if (Input.GetKey(KeyCode.D))
+        {
+            v.x = moveSpeed;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            v.x = -moveSpeed;
+        }
+        else
+        {
+            v.x = 0;
+        }
+        if (UnityEngine.Input.GetButton("Jump") || UnityEngine.Input.GetKey(KeyCode.Space))
+        {
+            if (Cube == true)
+            {
+                Debug.DrawRay(rayPosition, Vector3.down * distance, Color.red);
+                v.y = moveJump;
+            }
+            else
+            {
+                Debug.DrawRay(rayPosition, Vector3.down * distance, Color.yellow);
+            }
+        }
+        rb.velocity = v;
+    }
+}
