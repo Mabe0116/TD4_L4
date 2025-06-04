f463d920ecae94f97c75ea54d52f9ec28558fc3c
shibuehayato <115055046+shibuehayato@users.noreply.github.com>
2025-06-04 15:50:26 +0900

    Titleスクリプトが消えていたので作った

---
+++ Assets/Scenes/Scripts/TitleScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScript : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public float speed = 5f; // 点滅速度（小さいほどゆっくり）
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Map1");
+        }
+
+        //テキストの点滅
+        if (text != null)
+        {
+            float alpha = (Mathf.Sin(Time.time * speed) + 1f) / 2f;
+            Color c = text.color;
+            c.a = alpha;
+            text.color = c;
+        }
+    }
+}
