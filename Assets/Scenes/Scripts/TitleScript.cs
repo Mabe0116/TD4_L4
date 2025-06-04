3c7f0884bb08039002ba2718f19b16fefa38a0b2
k022g0004 <k022g0004@g.neec.ac.jp>
2025-06-04 14:54:19 +0900

    タイトル画面作成

--- Assets/Scenes/Scripts/TitleScript.cs
+++ Assets/Scenes/Scripts/TitleScript.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class TitleScript : MonoBehaviour
 {
+    public TextMeshProUGUI text;
+    public float speed = 5f; // 点滅速度（小さいほどゆっくり）
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +21,15 @@ public class TitleScript : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space))
         {
             SceneManager.LoadScene("Map1");
+        }
+
+        //テキストの点滅
+        if (text != null)
+        {
+            float alpha = (Mathf.Sin(Time.time * speed) + 1f) / 2f;
+            Color c = text.color;
+            c.a = alpha;
+            text.color = c;
         }
     }
 }
